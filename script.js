c736efc910f39e14286984517b96f4e3384e2720
Aaron Almendrala <afalmendrala16@gmail.com>
2023-03-05 04:39:06 +1100

    add function for 1 round game

--- script.js
+++ script.js
@@ -2,3 +2,43 @@ function getComputerChoice() {
     const choices = Array('Rock', 'Paper', 'Scissors');
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    switch(playerSelection.toLowerCase()) {
+        case 'rock':
+            if(computerSelection == 'Rock') {
+                return `Tie! Both players chose ${computerSelection}!`;
+            }
+            else if(computerSelection == 'Paper') {
+                return `You lose! ${computerSelection} beats ${playerSelection}!`;
+            }
+            else {
+                return `You win! ${playerSelection} beats ${computerSelection}!`;
+            }
+            break;
+        case 'paper':
+            if(computerSelection == 'Rock') {
+                return `You win! ${playerSelection} beats ${computerSelection}!`;
+            }
+            else if(computerSelection == 'Paper') {
+                return `Tie! Both players chose ${computerSelection}!`;
+            }
+            else {
+                return `You lose! ${computerSelection} beats ${playerSelection}!`;
+            }
+            break;
+        case 'scissors':
+            if(computerSelection == 'Rock') {
+                return `You lose! ${computerSelection} beats ${playerSelection}!`;
+            }
+            else if(computerSelection == 'Paper') {
+                return `You win! ${playerSelection} beats ${computerSelection}!`;
+            }
+            else {
+                return `Tie! Both players chose ${computerSelection}!`;
+            }
+            break;
+        default:
+            return 'Invalid choice. Please only select from the three';          
+    }
+}
