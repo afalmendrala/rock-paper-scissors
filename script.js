af47c273b5e96020843855cc86d84ce0e1264ba1
Aaron Almendrala <afalmendrala16@gmail.com>
2023-03-05 19:23:13 +1100

    add scores to game and playRound function

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let playerScore = 0
+    let computerScore = 0;
+    
     let playerWin = (playerSelection, computerSelection) => 
         `You win! ${playerSelection} beats ${computerSelection}!`;
     
@@ -16,45 +19,91 @@ function playRound(playerSelection, computerSelection) {
     switch(playerSelection.toLowerCase()) {
         case 'rock':
             if(computerSelection == 'Rock') {
-                return tie(computerSelection);
+                return [
+                    tie(computerSelection),
+                    playerScore,
+                    computerScore
+                ];
             }
             else if(computerSelection == 'Paper') {
-                return playerLose(playerSelection, computerSelection);
+                return [
+                    playerLose(playerSelection, computerSelection),
+                    playerScore,
+                    computerScore = computerScore + 1
+                ];
             }
             else {
-                return playerWin(playerSelection, computerSelection);
+                return [
+                    playerWin(playerSelection, computerSelection),
+                    playerScore = playerScore + 1,
+                    computerScore
+                ];
             }
         
         case 'paper':
             if(computerSelection == 'Rock') {
-                return playerWin(playerSelection, computerSelection);
+                return [
+                    playerWin(playerSelection, computerSelection),
+                    playerScore = playerScore + 1,
+                    computerScore
+                ];
             }
             else if(computerSelection == 'Paper') {
-                return tie(computerSelection);
+                return [
+                    tie(computerSelection),
+                    playerScore,
+                    computerScore
+                ];
             }
             else {
-                return playerLose(playerSelection, computerSelection);
+                return [
+                    playerLose(playerSelection, computerSelection),
+                    playerScore,
+                    computerScore = computerScore + 1
+                ];
             }
         
         case 'scissors':
             if(computerSelection == 'Rock') {
-                return playerLose(playerSelection, computerSelection);
+                return [
+                    playerLose(playerSelection, computerSelection),
+                    playerScore,
+                    computerScore = computerScore + 1
+                ];
             }
             else if(computerSelection == 'Paper') {
-                return playerWin(playerSelection, computerSelection);
+                return [
+                    playerWin(playerSelection, computerSelection),
+                    playerScore = playerScore + 1,
+                    computerScore
+                ];
             }
             else {
-                return tie(computerSelection);
+                return [
+                    tie(computerSelection),
+                    playerScore,
+                    computerScore
+                ];
             }            
     }
 }
 
 function game (){
-    let playerSelection; 
-    let computerSelection 
+    let playerSelection, computerSelection;
+    let playerScore = 0; 
+    let computerScore = 0;
+    let result, message;
     for (let i = 0; i < 5; i++) {
         playerSelection = prompt('Choice?');
         computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+
+        result = playRound(playerSelection, computerSelection);
+        message = result[0];
+
+        playerScore = playerScore + result[1];
+        computerScore = computerScore + result[2];
+
+        console.log(`${message} Player score: ${playerScore}, Computer score: ${computerScore}`);
+
     }
 }
