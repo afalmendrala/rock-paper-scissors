b4c0cbae734df746a3c7b468462c76deb8934091
Aaron Almendrala <afalmendrala16@gmail.com>
2023-03-08 00:56:24 +1100

    change game interface from console to UI

--- script.js
+++ script.js
@@ -87,3 +87,76 @@ function playRound(playerSelection, computerSelection) {
             }            
     }
 }
+
+function showGameWinner(playerScore, computerScore) {
+    if(playerScore > computerScore) {
+        return 'Player wins!';
+    }
+    else if(playerScore < computerScore) {
+        return 'Computer wins!';
+    }
+    else {
+        return 'Tie!';
+    }
+}
+
+function changeButtonsState(buttons) {
+    for(let i = 0; i < buttons.length; i++) {
+        if(buttons[i].disabled == false){
+            buttons[i].setAttribute('disabled', true);
+        }
+        else {
+            buttons[i].removeAttribute('disabled');
+        }
+    } 
+}
+
+const buttons = document.querySelectorAll('.choice');
+const body = document.querySelector('body');
+
+let roundResults;
+let playerScore = 0
+let computerScore = 0;
+let buttonClickCount = 0;
+
+let message = document.createElement('p');
+let scores = document.createElement('p');
+let winner = document.createElement('p');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttonClickCount = buttonClickCount + 1;
+
+        roundResults = playRound(button.value, getComputerChoice());
+        
+        message.textContent = roundResults[0];
+        body.appendChild(message);
+
+        playerScore = playerScore + roundResults[1];
+        computerScore = computerScore + roundResults[2];
+        scores.textContent = `Player score: ${playerScore}, Computer score: ${computerScore}`;
+        body.appendChild(scores);
+
+        if(buttonClickCount == 5){ 
+            winner.textContent = showGameWinner(playerScore, computerScore);
+            body.appendChild(winner);
+
+            changeButtonsState(buttons);
+            
+            const playAgain = document.createElement('button');
+            playAgain.textContent = 'Play again?'
+            body.appendChild(playAgain);
+            
+            playAgain.addEventListener('click', () => {
+                changeButtonsState(buttons);
+                playerScore = 0;
+                computerScore = 0;
+                buttonClickCount = 0;
+                body.removeChild(message);
+                body.removeChild(scores);
+                body.removeChild(winner);
+                body.removeChild(playAgain);
+            });
+        }
+    });
+});
